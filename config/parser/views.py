bad910b39235e3bab27f6dffdf63ed2afaed90af
IvanFoksha <v.foksh6@gmail.com>
2025-10-09 13:33:13 +0200

    Configurate media files handling for development

--- config/parser/views.py
+++ config/parser/views.py
@@ -5,12 +5,16 @@ from django.conf import settings
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.utils import timezone
-from django.views.generic import DetailView, FormView, ListView, View
+from django.views.generic import View
 from inertia import render
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector, SearchQuery, SearchRank
+)
 from django.db.models import F
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
 
 from config.parser.forms import ChannelParseForm
 from config.parser.models import TelegramChannel, Category, Country, Language
@@ -31,7 +35,12 @@ class ChannelSearchView(View):
 
         if query_param:
             # Обновление search_vector для поиска
-            TelegramChannel.objects.update(search_vector=(SearchVector('title', weight='A') + SearchVector('description', weight='B')))
+            TelegramChannel.objects.update(
+                search_vector=(
+                    SearchVector('title', weight='A') +
+                    SearchVector('description', weight='B')
+                )
+            )
 
             query = SearchQuery(query_param, search_type='websearch')
             queryset = queryset.annotate(
@@ -77,8 +86,12 @@ class ChannelSearchView(View):
             'avg_post_reach_24h': ('reach_24h_min', 'reach_24h_max'),
             'err': ('err_min', 'err_max'),
             'er': ('er_min', 'er_max'),
-            'male_audience_percentage': ('male_audience_min', 'male_audience_max'),
-            'female_audience_percentage': ('female_audience_min', 'female_audience_max'),
+            'male_audience_percentage': (
+                'male_audience_min', 'male_audience_max'
+            ),
+            'female_audience_percentage': (
+                'female_audience_min', 'female_audience_max'
+            ),
         }
 
         for db_field, url_params in range_filters.items():
@@ -124,15 +137,40 @@ class ChannelSearchView(View):
             'Channels/SearchPage',  # Имя React-компонента
             {
                 'channels': channels_list,
-                'filters': request.GET.dict()  # Передаем текущие фильтры на фронт
+                'filters': request.GET.dict()
             }
         )
 
 
-class ParserView(FormView):
+class ParserView(View):
+    """
+    {
+        "component": "Channels/ParsePage",
+        "props": {
+            "categories": [("news", "Новости и СМИ"), ...],
+            "form_errors": {"channel_identifier": ["This field is required."]}
+        },
+        "url": "/parser/parse/"
+    }
+    """
     form_class = ChannelParseForm
-    template_name = 'parser/parse_channel.html'
-    success_url = reverse_lazy("parser:list")
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(
+            request,
+            'Channels/ParsePage',
+            {
+                'categories': form.fields['category'].choices,
+            }
+        )
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
     def get_telegram_client(self):
         """Get Telegram client for parser work"""
@@ -186,21 +224,35 @@ class ParserView(FormView):
         """ Обработка формы """
         identifier = form.cleaned_data['channel_identifier']
         limit = form.cleaned_data['limit']
-        language = form.cleaned_data['language']
-        country = form.cleaned_data['country']
-        category = form.cleaned_data['category']
-        log.info(f'Начинаем обработку данных для канала; '
-                 f'- {identifier} лимит - {limit}')
+        # For now we get names, but we should probably move to FKs in the form
+        category_name = form.cleaned_data['category']
+        country_name = form.cleaned_data['country']
+        language_name = form.cleaned_data['language']
+
+        log.info(
+            f'Начинаем обработку данных для канала; - {identifier} лимит - {limit}'
+        )
         try:
             # Start async parsing function
             async_parser = async_to_sync(self.async_tg_parser)
             parsed_data = async_parser(identifier, limit)
-            parsed_data.update({'language': language.name if language else None,
-                                'country': country.name if country else None,
-                                'category': category.name if category else None})
 
-            log.info(f'Парсинг завершен для канала;'
-                     f'- {parsed_data.get("title")} ({parsed_data.get("channel_id")}')
+            # Here we need to get or create related objects
+            category, _ = Category.objects.get_or_create(name=category_name)
+            country, _ = Country.objects.get_or_create(
+                name=country_name,
+                defaults={'code': country_name[:2].upper()}
+            )
+            language, _ = Language.objects.get_or_create(name=language_name)
+
+            parsed_data.update({'language': language,
+                                'country': country,
+                                'category': category})
+
+            log.info(
+                f'Парсинг завершен для канала; - {parsed_data.get("title")} '
+                f'({parsed_data.get("channel_id")}'
+            )
 
             # Saving data
             channel, created = self.save_channel(parsed_data)
@@ -213,24 +265,118 @@ class ParserView(FormView):
             )
             messages.success(self.request, message)
 
-            return super().form_valid(form)
+            return HttpResponseRedirect(self.get_success_url())
 
         except Exception as e:
             form.add_error(None, str(e))
             return self.form_invalid(form)
 
+    def form_invalid(self, form):
+        """Rerender form with errors for Inertia"""
+        return render(
+            self.request,
+            'Channels/ParsePage',
+            {
+                'categories': form.fields['category'].choices,
+                'form_errors': form.errors.get_json_data()
+            }
+        )
 
-class ParserListView(ListView):
-    model = TelegramChannel
-    template_name = 'parser/channels_list.html'
-    context_object_name = "channels"
-    ordering = ["-parsed_at"]
+    def get_success_url(self):
+        return reverse_lazy("parser:list")
 
 
-class ParserDetailView(DetailView):
-    model = TelegramChannel
-    template_name = 'parser/channel_detail.html'
-    context_object_name = "channel"
+class ParserListView(View):
+    """
+    {
+        "component": "Channels/ListPage",
+        "props": {
+            "channels": [
+                {
+                    "id": 1,
+                    "name": "Example Channel",
+                    "subscribers": 1000,
+                    "parsed_at": "2025-10-09T12:00:00Z",
+                    "detail_url": "/parser/channel/1/"
+                }
+            ]
+        },
+        "url": "/parser/list/"
+    }
+    """
+    def get(self, request, *args, **kwargs):
+        channels = TelegramChannel.objects.order_by("-parsed_at")
+
+        channels_props = [
+            {
+                'id': channel.id,
+                'name': channel.title,
+                'subscribers': channel.subscribers_count,
+                'parsed_at': channel.parsed_at.isoformat(),
+                'detail_url': reverse_lazy(
+                    'parser:detail', kwargs={'pk': channel.pk}
+                )
+            }
+            for channel in channels
+        ]
+
+        return render(
+            request,
+            'Channels/ListPage',
+            {'channels': channels_props}
+        )
+
+
+class ParserDetailView(View):
+    """
+    {
+        "component": "Channels/DetailPage",
+        "props": {
+            "channel": {
+                "id": 1,
+                "name": "Example Channel",
+                "username": "example_channel",
+                "description": "A description.",
+                "subscribers": 1000,
+                "photo_url": "http://example.com/photo.jpg",
+                "category": "News",
+                "country": "Unknown",
+                "language": "English",
+                "avg_post_reach": 500,
+                "err": 50.0,
+                "is_verified": true,
+                "parsed_at": "2025-10-09T12:00:00Z"
+            }
+        },
+        "url": "/parser/channel/1/"
+    }
+    """
+    def get(self, request, *args, **kwargs):
+        channel = get_object_or_404(TelegramChannel, pk=kwargs['pk'])
+
+        channel_props = {
+            'id': channel.id,
+            'name': channel.title,
+            'username': channel.username,
+            'description': channel.description,
+            'subscribers': channel.subscribers_count,
+            'photo_url': channel.photo_url,
+            'category': getattr(channel.category, 'name', None),
+            'country': getattr(channel.country, 'name', None),
+            'language': getattr(channel.language, 'name', None),
+            'avg_post_reach': channel.avg_post_reach,
+            'err': channel.err,
+            'is_verified': channel.is_verified,
+            'parsed_at': (
+                channel.parsed_at.isoformat() if channel.parsed_at else None
+            ),
+        }
+
+        return render(
+            request,
+            'Channels/DetailPage',
+            {'channel': channel_props}
+        )
 
 
 # Create your views here.
--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from config.views import IndexView
 
@@ -29,3 +31,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
