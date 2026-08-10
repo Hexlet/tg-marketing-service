7df3c1ccd72b19419f69d059c0b4302bac740bc4
Maria Bazhenova <yankeevsdixie@gmail.com>
2026-08-11 00:24:08 +0300

    fix: fix import from Header

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,48 +1,47 @@
-import { Avatar, Box, Burger, Button, Flex, Text, Title } from '@mantine/core';
-import { useNavigate } from 'react-router-dom';
-import { SearchBar } from '../ui/SearchBar';
+import { Avatar, Box, Burger, Button, Flex, Text, Title } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
+import { SearchBar } from "../ui/SearchBar";
 
 interface HeaderProps {
-    opened: boolean;
-    onToggle: () => void;
-    showSearchBar?: boolean;
+ opened: boolean;
+ onToggle: () => void;
+ showSearchBar?: boolean;
 }
 
 const Header = ({ opened, onToggle, showSearchBar = true }: HeaderProps) => {
-    const navigate = useNavigate();
+ const navigate = useNavigate();
 
-    return (
-        <Flex px="md" py="sm" gap="sm" justify="space-between">
-            <Box>
-                <Burger
-                    hiddenFrom="sm"
-                    opened={opened}
-                    onClick={onToggle}
-                />
+ return (
+  <Flex px="md" py="sm" gap="sm" justify="space-between">
+   <Box>
+    <Burger hiddenFrom="sm" opened={opened} onClick={onToggle} />
 
-                <Box visibleFrom="sm">
-                    <Flex gap="sm" align="center">
-                        <Avatar color="tgblue" radius="md">t</Avatar>
-                        <Title order={3}>tgpulse</Title>
-                    </Flex>
-                </Box>
-            </Box>
+    <Box visibleFrom="sm">
+     <Flex gap="sm" align="center">
+      <Avatar color="tgblue" radius="md">
+       t
+      </Avatar>
+      <Title order={3}>tgpulse</Title>
+     </Flex>
+    </Box>
+   </Box>
 
-            <Flex wrap="wrap-reverse" gap="5px">
-                {showSearchBar && (
-                    <Box flex="1 1 0" miw="300">
-                        <SearchBar />
-                    </Box>
-                )}
-                <Flex align="center" gap="sm" ml="auto" style={{ flexShrink: 0 }}>
-                    <Button variant="subtle" onClick={() => navigate('/auth')}>Войти</Button>
-                    <Text>Мария Л.</Text>
-                    <Avatar color="pink">M</Avatar>
-                </Flex>
-            </Flex>
-        </Flex>
-    )
-
-}
+   <Flex wrap="wrap-reverse" gap="5px">
+    {showSearchBar && (
+     <Box flex="1 1 0" miw="300">
+      <SearchBar />
+     </Box>
+    )}
+    <Flex align="center" gap="sm" ml="auto" style={{ flexShrink: 0 }}>
+     <Button variant="subtle" onClick={() => navigate("/auth")}>
+      Войти
+     </Button>
+     <Text>Мария Л.</Text>
+     <Avatar color="pink">M</Avatar>
+    </Flex>
+   </Flex>
+  </Flex>
+ );
+};
 
 export default Header;
