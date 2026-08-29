fca45ef5fb83a8fc6c7533826a242376bb810ba6
cheshire <siamskymug@yandex.kz>
2026-08-29 22:21:43 +0300

    corrected legal tests

--- apps/legal/test_legal.py
+++ apps/legal/test_legal.py
@@ -20,13 +20,25 @@ DOCUMENTS_FIXTURE = {
 @pytest.mark.django_db
 def test_documents_view(client):
     # GET-запрос
-    response = client.get("/legal/")
+    response = client.get(
+        "/legal/",
+        HTTP_ACCEPT="application/json",
+        HTTP_X_INERTIA="true",
+    )
 
     assert response.status_code == 200
+
+    data = response.json()
+    assert data["component"] == "Legal"
+    assert data["props"]["documents"] == DOCUMENTS_FIXTURE
 
 
 class LegalViewTestCase(InertiaTestCase):
     def test_show_assertions(self):
-        self.client.get("/legal/")
+        self.client.get(
+            "/legal/",
+            HTTP_ACCEPT="application/json",
+            HTTP_X_INERTIA="true",
+        )
 
         self.assertComponentUsed("Legal")
