b8ec6c76db3324728944baf89a80ded3ea5b6644
cheshire <siamskymug@yandex.kz>
2026-09-06 21:03:49 +0300

    corrected role request

--- apps/users/middleware.py
+++ apps/users/middleware.py
@@ -5,7 +5,6 @@ from typing import cast
 from django.contrib.auth import get_user_model
 from django.http import HttpRequest, HttpResponse
 
-from apps.users.roles import Role
 
 logger = logging.getLogger(__name__)
 User = get_user_model()
@@ -23,21 +22,16 @@ class RoleMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
-        # Получаем роль пользователя, если она известна
-        user_role = getattr(request.user, "role", None)
-        role_obj = Role._default_manager.filter(code=user_role).first()
-
-        # Если роль пользователя не найдена, ставим гостевую роль
-        if role_obj:
-            final_role = role_obj.code
-        elif user_role:
-            final_role = user_role
-        else:
-            final_role = "guest"
-
-        # Ставим роль в запрос
+        """
+        Получаем роль пользователя, если она известна
+        Если роль пользователя не найдена, ставим гостевую роль
+        """
         role_request = cast(RoleRequest, request)
-        role_request.role = final_role
+
+        if request.user.is_authenticated:
+            request.role = request.user.role
+        else:
+            request.role = "guest"
 
         # logging-сообщение
         logger.debug(
